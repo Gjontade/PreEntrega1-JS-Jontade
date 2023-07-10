31dae23925ab87b990c43b1624b4a09a6c0725a2
Gfjontade <gonzajontade@gmail.com>
2023-07-10 18:54:05 -0300

    Correccion de funcionamiento de la funcion 'seleccionarAlimento'

--- main.js
+++ main.js
@@ -13,46 +13,50 @@ function comprarEntradas() {
 
 	if (nombre === "") {
 		alert("Debe ingresar un nombre para continuar con la compra.");
-		return;
-	} // return interrumpe la ejecucion
+		return; // return interrumpe la ejecucion
+	} else {
+		document.write("Hola " + nombre + ". <br>Detalle de tu compra: ");
 
-	document.write("Hola " + nombre + ". <br>Detalle de tu compra: ");
+		pelicula = prompt(
+			"Selecciona la pelicula: Jurassic Park | Rocky | titanic").toUpperCase();
+		if (pelicula == "JURASSIC PARK") {
+			document.write("<br>La pelicula seleccionada es: " + pelicula + ". <br>");
+		} else if (pelicula == "ROCKY") {
+			document.write("<br>La pelicula seleccionada es: " + pelicula + ". <br>");
+		} else if (pelicula == "TITANIC") {
+			document.write("<br>La pelicula seleccionada es: " + pelicula + ". <br>");
+		} else {
+			alert("¡Comando Incorrecto!");
+			return;
+		}
 
-	pelicula = prompt("Selecciona la pelicula: Jurassic Park | Rocky | titanic");
-    if (pelicula == "jurassic park"){
-	    document.write("<br>La pelicula seleccionada es: " + pelicula + ". <br>");
-    } else if (pelicula == "rocky"){
-        document.write("<br>La pelicula seleccionada es: " + pelicula + ". <br>");
-    } else if (pelicula == "titanic"){
-        document.write("<br>La pelicula seleccionada es: " + pelicula + ". <br>");
-    } else {
-        alert("¡Comando Incorrecto!");
-        return;
-    }
+		asientos = Number(prompt("Cantidad de asientos: (valor: $500 c/u)"));
+		valor(precioEntrada, asientos);
 
-	asientos = Number(prompt("Cantidad de asientos: (valor: $500 c/u)"));
-	valor(precioEntrada, asientos);
-};
-
-function seleccionarAlimento() {
-    var itemsDisponibles = ["Pochoclos", "Chocolate", "Gaseosa", "Agua"];
-    var carrito = [];
-
-    var i = 0;
-    while (i < itemsDisponibles.length) {
-    var seleccion = confirm("¿Deseas agregar a la reserva " + itemsDisponibles[i] + " para acompañar la pelicula?");
-
-    if (seleccion) {
-        carrito.push(itemsDisponibles[i]);
-    }
-
-    i++;
+		seleccionarAlimento();
+	}
 }
+function seleccionarAlimento() {
+	var itemsDisponibles = ["Pochoclos", "Chocolate", "Gaseosa", "Agua"];
+	var carrito = [];
 
-document.write("<br>Reserva: <br>" + carrito + ". (Podes abonar y retirar por el candy hasta 5 min antes de la pelicula).");
+	var i = 0;
+	while (i < itemsDisponibles.length) {
+		var seleccion = confirm(
+			"¿Deseas agregar a la reserva " + itemsDisponibles[i] + " para acompañar la pelicula?"
+		);
 
-};
+		if (seleccion) {
+			carrito.push(itemsDisponibles[i]);
+		}
+
+		i++;
+	}
+
+	document.write(
+		"<br>Reserva: <br>" + carrito + ". (Podes abonar y retirar por el candy hasta 5 min antes de la pelicula)."
+	);
+}
 
 // Ejecucion
 comprarEntradas();
-seleccionarAlimento();
